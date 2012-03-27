8250a559a7bf98e0ec84884132592f9e635ed590
TAKANO Mitsuhiro <takano32@gmail.com>
2012-03-27 21:54:51 +0900

    impl fb

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -2,7 +2,21 @@
 
 object FizzBuzz {
 	def main(args: Array[String]) {
-		println("Hello, World!")
+		// println("Hello, World!")
+		fb(100)
+	}
+
+	def fb(n: Int) {
+		if (n == 0) return
+		fb(n - 1)
+		if (n % 15 == 0)
+			println("FizzBuzz")
+		else if (n % 5 == 0)
+			println("Buzz")
+		else if (n % 3 == 0)
+			println("Fizz")
+		else
+			println(n)
 	}
 }
 
